96463add9c3f2604febb2abb362e59bb578fb478
Victor Moraes <vct.aragao@gmail.com>
2023-07-22 12:28:06 -0300

    chore: test seeking

--- server/basic.c
+++ server/basic.c
@@ -12,10 +12,13 @@
  #include <mpg123.h>
  #include <stdio.h>
  #include <strings.h>
- 
+
+
+#define SECONDS 16  
+
  void usage(const char *cmd)
  {
-  printf("Usage: %s <input> [<driver> [<output> [encoding [buffersize]]]]\n" , cmd);
+  printf("Usage: %s <input> [<offset> [<driver> [<output> [encoding [buffersize]]]]]\n" , cmd);
   printf( "\nPlay MPEG audio from intput file to output file/device using\n"
   "specified out123 driver, sample encoding and buffer size optional.\n\n" );
   exit(99);
@@ -39,6 +42,7 @@
   unsigned char* buffer = NULL;
   const char *encname;
   size_t buffer_size = 0;
+  size_t buffer_size_default = 0;
   size_t done = 0;
   int channels = 0;
   int encoding = 0;
@@ -46,23 +50,28 @@
   long rate = 0;
   int err = MPG123_OK;
   off_t samples = 0;
+
+  off_t offset = 0;
+  int offset_seconds = 0;
  
   if(argc<2) usage(argv[0]);
  
   infile = argv[1];
 
-  if(argc >= 3) driver = argv[2];
+  if(argc >= 3) offset_seconds = atoi(argv[2]);
+  printf("[debug] offset_seconds: %d\n" , offset_seconds);
+
   if(argc >= 4) outfile = argv[3];
 
   printf("Input file: %s\n", infile);
   printf("Output driver: %s\n", driver ? driver : "<nil> (default)");
   printf("Output file: %s\n", outfile ? outfile : "<nil> (default)");
- 
- #if MPG123_API_VERSION < 46
+
+#if MPG123_API_VERSION < 46
   // Newer versions of the library don't need that anymore, but it is safe
   // to have the no-op call present for compatibility with old versions.
   err = mpg123_init();
- #endif
+#endif
 
   mh = mpg123_new(NULL, &err);
   if(err != MPG123_OK || mh == NULL)
@@ -127,38 +136,53 @@
       return -1;
   }
 
-  /* Buffer could be almost any size here, mpg123_outblock() is just some
-     recommendation. The size should be a multiple of the PCM frame size. */
+  long one_second = rate * framesize;
+  printf("[debug] one_second: %ld\n", one_second); // 176_400
+                                                   
+  /* seek from desired offset */
+  off_t sample_off = one_second * offset_seconds;
+  printf("[debug] sample_off: %ld\n", sample_off); // 2_822_400
+  offset = mpg123_seek(mh, sample_off, SEEK_SET);
 
-  /* 
-   * 1s of music = rate Hz * framezise 
+  if(offset < 0 ){
+      fprintf(stderr, "Offset not able to be reached: %s\n" , mpg123_strerror(mh));
+      cleanup(mh, ao);
+      return -1;
+  }
+  printf("[debug] offset: %ld\n", offset); // 2_822_400
+                                           
+  /* Buffer could be almost any size here, mpg123_outblock() is just some
+   * recommendation. The size should be a multiple of the PCM frame size.
    * 1s of music in buffer = default_buffer * ( 1s of music / default_buffer) 
    * 15s of music in buffer = 1s of music in buffer * 15
    */
-  buffer_size = argc >= 6 ? atol(argv[5]) : mpg123_outblock(mh) * 575;
-  printf("[debug] buffer_size: %ld\n", buffer_size);
+  buffer_size_default = argc >= 6 ? atol(argv[5]) : mpg123_outblock(mh);
+  printf("[debug] buffer_size_default: %ld\n", buffer_size_default); // 4_608 
+  buffer_size = buffer_size_default * ((one_second / buffer_size_default) * SECONDS);
+
+  printf("[debug] buffer_size: %ld\n", buffer_size); // 2_646_000
   buffer = malloc( buffer_size );
 
-//  do
-//  {
-//      size_t played;
-//      err = mpg123_read( mh, buffer, buffer_size, &done );
-//      played = out123_play(ao, buffer, done);
-//      if(played != done)
-//      {
-//          fprintf(stderr
-//                  , "Warning: written less than gotten from libmpg123: %li != %li\n"
-//                  , (long)played, (long)done);
-//      }
-//      printf("[debug] played: %ld\n", played);
-//      printf("[debug] framesize: %d\n", framesize);
-//      samples += played/framesize;
-//      printf("[debug] sample: %ld\n\n", samples);
-//      /* We are not in feeder mode, so MPG123_OK, MPG123_ERR and
-//         MPG123_NEW_FORMAT are the only possibilities.
-//         We do not handle a new format, MPG123_DONE is the end... so
-//         abort on anything not MPG123_OK. */
-//  } while (done && err==MPG123_OK);
+  //  do
+  //  {
+  //      size_t played;
+  //      err = mpg123_read( mh, buffer, buffer_size, &done );
+  //      played = out123_play(ao, buffer, done);
+  //      if(played != done)
+  //      {
+  //          fprintf(stderr
+  //                  , "Warning: written less than gotten from libmpg123: %li != %li\n"
+  //                  , (long)played, (long)done);
+  //      }
+  //      printf("[debug] played: %ld\n", played);
+  //      printf("[debug] framesize: %d\n", framesize);
+  //      samples += played/framesize;
+  //      printf("[debug] sample: %ld\n\n", samples);
+  //      /* We are not in feeder mode, so MPG123_OK, MPG123_ERR and
+  //         MPG123_NEW_FORMAT are the only possibilities.
+  //         We do not handle a new format, MPG123_DONE is the end... so
+  //         abort on anything not MPG123_OK. */
+  //  } while (done && err==MPG123_OK);
 
   size_t played;
   err = mpg123_read( mh, buffer, buffer_size, &done );
